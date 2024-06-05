59031de5b7c6c56d61a5eccc13383497f4bfdce9
Michael Silva <michael.sigma.3@gmail.com>
2024-06-05 20:06:19 -0300

    Teste

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Curso de canto</h1>
+        <p>Teste de publicacao</p>
         
       </header>
     </div>
