40f0d2301f5db81a1af43a2d179fe61fefbf646d
Michael Silva <michael.sigma.3@gmail.com>
2024-06-05 21:02:13 -0300

    Utilizando roteamento

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes , Route } from 'react-router-dom';
 
-function App() {
+const Sobre = () => <h1>Sobre</h1>;
+
+const App  = () => {
+  return (
+    <BrowserRouter>
+      <Routes >
+        <Route exact path="/" component={() => <Home />} />
+        <Route path="/sobre" component={() => <Sobre />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Curso de canto</h1>
         <p>Teste de publicacao</p>
-        
       </header>
     </div>
   );
